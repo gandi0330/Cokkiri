40ba62c46a120d5abcbe8d0b1e0b10a37e041078
Wise Programmer <67681207+gandi0330@users.noreply.github.com>
2022-07-19 11:03:09 +0900

    feat : S07P12C107-33 환경설정

---
+++ backend/src/main/java/corinee/cokkiri/CokkiriApplication.java
@@ -0,0 +1,13 @@
+package corinee.cokkiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokkiriApplication {
+	// 현명 테스트 완료
+	public static void main(String[] args) {
+		SpringApplication.run(CokkiriApplication.class, args);
+	}
+
+}
