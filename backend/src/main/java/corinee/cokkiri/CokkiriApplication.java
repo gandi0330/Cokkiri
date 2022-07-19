2ce195f73047dff2c5532cf0aabda9591c2a70b9
mssdfg0708 <mssdfg0708@naver.com>
2022-07-19 11:14:47 +0900

    feat : S07P12C107-34 Spring 환경 세팅

--- backend/src/main/java/corinee/cokkiri/CokkiriApplication.java
+++ backend/src/main/java/corinee/cokkiri/CokkiriApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CokkiriApplication {
 	// 현명 테스트 완료
+	// 동완 테스트 완료
 	public static void main(String[] args) {
 		SpringApplication.run(CokkiriApplication.class, args);
 	}
