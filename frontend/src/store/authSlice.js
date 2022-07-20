3ff0aa29d06ee6609ce6de7db367f325fccdbf82
yunjeongwon <yunjwon94@gmail.com>
2022-07-20 17:53:58 +0900

    feat : S07P12C107-60 회원 redux 설정

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -1,37 +1,75 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  loginLoading: false,
+  loginDone: false,
+  loginError: null,
+
+  logoutLoading: false,
+  logoutDone: false,
+  logoutError: null,
+
+  signupEmailLoading: false,
+  signupEmailDone: false,
+  signupEmailError: null,
+
+  signupCertificationLoading: false,
+  signupCertificationDone: false,
+  signupCertificationError: null,
+
+  signupDetailLoading: false,
+  signupDetailDone: false,
+  signupDetailError: null,
+};
+
+const login = createAsyncThunk(
+  'auth/login',
+  async ({ email, password }) => {
+    try {
+      const res = await axios.post('http://', { email, password }, {
+        headers: {},
+      });
+
+      const data = await response.json();
+      console.log('data', data);
+
+      if (res.status === 200) {
+        localStorage.setItem('token', data.token);
+        return { ...data, email, password };
+      }
+      return thunkAPI.rejectWithValue(data);
+    } catch (error) {
+      console.error(error.response.data);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
 const authSlice = createSlice({
   name: 'auth',
-  initialState: {
-    isLoggedIn: false,
-    isRequestedCertif: false,
-    isCertificated: false,
-    isSignedUp: false,
-  },
-  reducers: {
-    login: (state, action) => {
-      console.log(action.payload);
-      state.isLoggedIn = true;
-    },
-    logout: (state) => {
-      state.isLoggedIn = false;
-    },
-    reqCertificationNumber: (state) => {
-      state.isRequestedCertif = true;
-    },
-    certificateNumber: (state, action) => {
-      console.log(action.payload);
-      state.isCertificated = true;
-    },
-    signup: (state, action) => {
-      console.log(action.payload);
-      state.isSignedUp = true;
-    },
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state) => {
+      state.loginLoading = true;
+      state.loginDone = false;
+      state.loginError = null;
+    });
+    builder.addCase(login.fulfilled, (state) => {
+      state.loginLoading = false;
+      state.loginDone = true;
+      state.loginError = null;
+    });
+    builder.addCase(login.rejected, (state, { payload }) => {
+      state.loginLoading = false;
+      state.loginDone = false;
+      state.loginError = payload;
+    });
   },
 });
 
 export const {
-  login, logout, reqCertificationNumber, certificateNumber, signup,
+  loginRequest,
 } = authSlice.actions;
 
 export default authSlice.reducer;
