8b147839867984bbebc9b288ada90ee4e90e6b52
정혜령 <jhr3681@naver.com>
2022-07-21 14:45:36 +0900

    feat : S07P12C107-39 레이아웃-네비게이션

--- frontend/src/components/layout/NavBar.js
+++ frontend/src/components/layout/NavBar.js
@@ -1,17 +1,63 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+import { MdOutlineAccountCircle } from 'react-icons/md';
+import classes from './NavBar.module.css';
+import Elephant from '../../images/Elephant.png';
+
 const NavBar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
   const { isLoggedIn } = useSelector((state) => state.auth);
 
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/my-record">공부기록</NavLink>
-      <NavLink to="/rooms">스터디 룸</NavLink>
-      <NavLink to="/my-page">마이 페이지</NavLink>
-      { !isLoggedIn && <button type="button"><NavLink to="/login">login</NavLink></button>}
-      { isLoggedIn && <button type="button"><NavLink to="/logout">logout</NavLink></button>}
+    <nav className={classes.nav}>
+      <ul>
+        <div className={classes.navItems}>
+          <li id="logo" className={classes.logo}>
+            <img src={Elephant} alt="cokkiri logo" />
+            <NavLink to="/">Co:kkiri</NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/my-record"
+              className={classes.navItem}
+              style={({ isActive }) => ({ color: isActive ? '#587291' : '#B3B6B7' })}
+            >
+              공부기록
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/rooms"
+              className={classes.navItem}
+              style={({ isActive }) => ({ color: isActive ? '#587291' : '#B3B6B7' })}
+            >
+              스터디 룸
+            </NavLink>
+          </li>
+        </div>
+        <li>
+          {isLoggedIn && <Link to="/login">로그인</Link>}
+          {!isLoggedIn && (
+            <MdOutlineAccountCircle
+              size="2.5rem"
+              className={classes.accountIcon}
+              onClick={() => setShowDropdown((prevState) => !prevState)}
+            />
+          )}
+          {showDropdown && (
+            <div className={classes.dropdown}>
+              <div className={classes.dropdownMenu}>
+                <div>닉네임: ssafyforever</div>
+                <div>이메일: ssafy@naver.com</div>
+                <Link to="/logout">비밀번호 변경</Link>
+                <Link to="/logout">로그아웃</Link>
+              </div>
+            </div>
+          )}
+        </li>
+      </ul>
     </nav>
   );
 };
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,6 +1,57 @@
+import Timer from '../images/Timer.jpg';
+import Siren from '../images/Siren.jpg';
+import classses from './MainPage.module.css';
+
 const MainPage = () => {
   return (
-    <div>MainPage</div>
+    <>
+      <header className={classses.header}>
+        <div className={classses.heroText}>
+          <h1>우리 코드 끼리 끼리</h1>
+          <h1>Co:kkiri</h1>
+        </div>
+      </header>
+      <main className={classses.main}>
+        <section className={classses.section}>
+          <img src={Timer} alt="타이머 기능 소개 이미지" />
+          <div>
+            <h3>타이머 기능</h3>
+            <p>그동안 일일이 타이머 찾기 귀찮으셨죠?</p>
+            <p>
+              코끼리에서는 알고리즘 문제를
+              같이 시간을 재며 풀 수 있어요!
+            </p>
+          </div>
+        </section>
+        <section className={classses.section}>
+          <div>
+            <h3>사이렌 기능</h3>
+            <p>
+              친구가 코드 짜다가 사라져 버렸다고요?
+            </p>
+            <p>
+              그때는 사이렌 기능을 이용해 친구를 불러오세요!
+            </p>
+          </div>
+          <img src={Siren} alt="사이렌 기능 소개 이미지" />
+        </section>
+      </main>
+      <footer className={classses.footer}>
+        <hr />
+        <div>
+          <span>Developers</span>
+          <p>코린이</p>
+        </div>
+        <div>
+          <span>Contact</span>
+          <p>cokkiri@naver.com</p>
+        </div>
+        <div>
+          <span>Company</span>
+          <p>&copy;SSAFY</p>
+        </div>
+      </footer>
+    </>
   );
 };
 
