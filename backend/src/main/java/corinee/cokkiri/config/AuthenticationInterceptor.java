901449756d49e0203c52f95626ad36a4ab5d347f
정현명 <gandi0330@naver.com>
2022-07-22 15:56:07 +0900

    feat jwt인터셉터개발 be 정현명

---
+++ backend/src/main/java/corinee/cokkiri/config/AuthenticationInterceptor.java
@@ -0,0 +1,39 @@
+package corinee.cokkiri.config;
+
+import corinee.cokkiri.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        try{
+            String token = request.getHeader("X-access-token");
+            if(token == null || !jwtTokenUtil.verifyToken(token)){
+                response.setStatus(401);
+                response.getWriter().write("access-token-invalid");
+                return false;
+            };
+
+            response.setStatus(200);
+            return true;
+
+        }catch(Exception e){
+            response.setStatus(500);
+        }
+
+        return true;
+
+    }
+
+}
