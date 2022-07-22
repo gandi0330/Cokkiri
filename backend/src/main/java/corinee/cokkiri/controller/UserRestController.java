4bb4294d3dc607d2e3eec32b02ac68df9b49bd8d
정현명 <gandi0330@naver.com>
2022-07-22 17:22:33 +0900

    feat 로그인api be 정현명

--- backend/src/main/java/corinee/cokkiri/controller/UserRestController.java
+++ backend/src/main/java/corinee/cokkiri/controller/UserRestController.java
@@ -2,30 +2,49 @@ package corinee.cokkiri.controller;
 
 import corinee.cokkiri.common.Result;
 import corinee.cokkiri.domain.User;
+import corinee.cokkiri.request.UserLoginRequest;
 import corinee.cokkiri.response.FindUserResponse;
+import corinee.cokkiri.response.UserLoginResponse;
 import corinee.cokkiri.service.UserService;
+import corinee.cokkiri.util.JwtTokenUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class UserRestController {
 
     private final UserService userService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @PostMapping("/user")
+    public ResponseEntity<? extends Result> loginUser(@RequestBody UserLoginRequest userLoginRequest){
+        String email = userLoginRequest.getEmail();
+        String password = userLoginRequest.getPassword();
+        User user =userService.findByEmail(email);
+
+        if(user == null){
+            return ResponseEntity.status(404).body(Result.of(404,"유저가 존재하지 않습니다"));
+        }
+
+        if(!password.equals(user.getPassword())){
+            return ResponseEntity.status(401).body(Result.of(401,"비밀번호가 일치하지 않습니다"));
+        }
+
+        return ResponseEntity.ok(UserLoginResponse.of(200,"success", jwtTokenUtil.createAccessToken("email",user.getEmail()), user.getEmail()));
+
+    }
 
     @GetMapping("/user/info/{user_email}")
-    public ResponseEntity<? extends Result<FindUserResponse>> findUser(@PathVariable("user_email") String email) {
+    public ResponseEntity<? extends Result> findUser(@PathVariable("user_email") String email) {
         User findUser = userService.findByEmail(email);
-        FindUserResponse response = new FindUserResponse(findUser.getEmail(), findUser.getNickname());
-        if (findUser.getEmail() == null)
-            return ResponseEntity.status(404).body( new Result<>(404, response));
+        if (findUser == null)
+            return ResponseEntity.status(404).body(Result.of(401,"유저가 존재하지 않습니다"));
         else
-            return ResponseEntity.status(200).body( new Result<>(200, response));
+            return ResponseEntity.status(200).body(FindUserResponse.of(200,"success",findUser));
     }
 
 }
