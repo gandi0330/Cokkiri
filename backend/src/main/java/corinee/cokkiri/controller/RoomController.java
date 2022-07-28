264f8ccc8b8727d93e59d3c7501a44956d71ed55
mssdfg0708 <mssdfg0708@naver.com>
2022-07-28 18:06:24 +0900

    feat : S07P12C107-160 스터디룸 입장

--- backend/src/main/java/corinee/cokkiri/controller/RoomController.java
+++ backend/src/main/java/corinee/cokkiri/controller/RoomController.java
@@ -3,6 +3,8 @@ package corinee.cokkiri.controller;
 import corinee.cokkiri.common.Result;
 import corinee.cokkiri.domain.Room;
 import corinee.cokkiri.request.CreateRoomRequest;
+import corinee.cokkiri.request.EnterRoomRequest;
+import corinee.cokkiri.response.EnterRoomResponse;
 import corinee.cokkiri.response.FindRoomListResponse;
 import corinee.cokkiri.response.FindRoomResponse;
 import corinee.cokkiri.service.RoomService;
@@ -14,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -41,7 +44,7 @@ public class RoomController {
     }
 
     @ApiOperation(value = "스터디룸 목록 조회", notes = "[offset : 페이지 시작점]    [limit : 1페이당 정보 개수]")
-    @GetMapping("room/{offset}/{limit}")
+    @GetMapping("/room/{offset}/{limit}")
     @ApiResponses({
             @ApiResponse(code=200, message = "성공"),
             @ApiResponse(code=404, message = "스터디룸이 존재하지 않습니다"),
@@ -66,5 +69,15 @@ public class RoomController {
             return ResponseEntity.status(404).body(Result.of(404, "스터디룸이 존재하지 않습니다"));
         return ResponseEntity.status(200).body(FindRoomResponse.of(200, "스터디룸 목록 조회 성공",
                 room));
+    }
+
+    @ApiOperation(value = "스터디룸 입장", notes = "Response 로 응답된 [정수 index] 저장, 스터디룸 퇴장 요청 때 다시 전달 해주세요")
+    @PostMapping("/room/entrance")
+    public ResponseEntity<? extends Result> enterRoom(@RequestBody @Valid EnterRoomRequest request) {
+        Long index = roomService.enterRoom(request);
+        if (index == -1) {
+            return ResponseEntity.status(500).body(Result.of(500, "스터디룸 입장 실패"));
+        }
+        return ResponseEntity.status(200).body(EnterRoomResponse.of(200, "스터디룸 입장 성공", index));
     }
 }
