09dc98e89d7cc8e920ee4881c354e4f52ac62635
정현명 <gandi0330@naver.com>
2022-07-28 16:37:34 +0900

    feat : S07P12C107-148 질문 상세조회 API 개발

---
+++ backend/src/main/java/corinee/cokkiri/response/GetQuestionResponse.java
@@ -0,0 +1,32 @@
+package corinee.cokkiri.response;
+
+import corinee.cokkiri.common.Result;
+import corinee.cokkiri.domain.Question;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class GetQuestionResponse extends Result {
+    private Long questionId;
+    private String title;
+    private String content;
+    private Long roomId;
+    private String questionWriterEmail;
+    private LocalDateTime create_dateTime;
+
+    public static GetQuestionResponse of (int statusCode, String message, Question question){
+        GetQuestionResponse res = new GetQuestionResponse();
+        res.setStatusCode(statusCode);
+        res.setMessage(message);
+        res.setQuestionId(question.getQuestionId());
+        res.setTitle(question.getTitle());
+        res.setContent(question.getContent());
+        res.setRoomId(question.getRoom().getRoomId());
+        res.setQuestionWriterEmail(question.getUser().getEmail());
+        res.setCreate_dateTime(question.getCreateDatetime());
+
+        return res;
+    }
+}
