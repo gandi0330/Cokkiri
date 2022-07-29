f032cdd15c4c693076b30cce71a9a66f047c6d87
Wise Programmer <67681207+gandi0330@users.noreply.github.com>
2022-07-29 11:25:59 +0900

    feat : S07P12C107-155 답변 상세조회 API 개발

--- backend/src/main/java/corinee/cokkiri/controller/AnswerController.java
+++ backend/src/main/java/corinee/cokkiri/controller/AnswerController.java
@@ -1,10 +1,13 @@
 package corinee.cokkiri.controller;
 
 import corinee.cokkiri.common.Result;
+import corinee.cokkiri.domain.Answer;
 import corinee.cokkiri.domain.Question;
 import corinee.cokkiri.domain.Room;
 import corinee.cokkiri.request.AnswerAddRequest;
 import corinee.cokkiri.request.QuestionAddRequest;
+import corinee.cokkiri.response.GetAnswerResponse;
+import corinee.cokkiri.response.GetQuestionResponse;
 import corinee.cokkiri.service.AnswerService;
 import corinee.cokkiri.service.QuestionService;
 import corinee.cokkiri.service.RoomService;
@@ -14,10 +17,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -49,6 +49,21 @@ public class AnswerController {
 
         return ResponseEntity.status(200).body(Result.of(200,"success"));
 
+    }
+
+    @GetMapping("/answer/detail/{answer_id}")
+    @ApiOperation(value="답변 상세조회", notes="답변 하나를 조회한다")
+    @ApiResponses({
+            @ApiResponse(code=200, message="성공"),
+            @ApiResponse(code=404, message="답변이 존재하지 않음"),
+            @ApiResponse(code=500, message="서버 오류")
+    })
+    public ResponseEntity<? extends Result> getAnswer(@PathVariable("answer_id") Long answerId){
+        Answer answer = answerService.getAnswer(answerId);
+
+        if(answer == null)
+            return ResponseEntity.status(404).body(Result.of(404, "답변이 존재하지 않습니다"));
+        return ResponseEntity.status(200).body(GetAnswerResponse.of(200,"success",answer));
     }
 
 }
