9a98e8c4991416231676bafe344b1b2ed4009436
gandi0330 <gandi0330@naver.com>
2022-08-01 16:11:16 +0900

    feat : S07P12C107-151 답변목록조회 API 개발

--- backend/src/main/java/corinee/cokkiri/repository/AnswerRepository.java
+++ backend/src/main/java/corinee/cokkiri/repository/AnswerRepository.java
@@ -5,12 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class AnswerRepository {
     private final EntityManager em;
 
+
+    public List<Answer> getAnswerList(Long questionId){
+        return em.createQuery("select a from Answer a where a.question.questionId=:questionId",Answer.class)
+                .setParameter("questionId",questionId)
+                .getResultList();
+    }
 
     public Long save(Answer answer){
         em.persist(answer);
