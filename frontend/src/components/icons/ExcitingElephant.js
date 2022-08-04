5301e7c8579d4394ac19afd85867f51551fe310c
정혜령 <jhr3681@naver.com>
2022-08-04 11:54:50 +0900

    feat : S07P12C107-186 질문 게시판 퍼블리싱

---
+++ frontend/src/components/icons/ExcitingElephant.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import elephant from '../../images/ExcitingElephant.svg';
+
+const ExcitingElephant = () => {
+  return <img src={elephant} alt="exiciting Elephant" />;
+};
+
+export default ExcitingElephant;
--- frontend/src/pages/questions/QuestionPage.js
+++ frontend/src/pages/questions/QuestionPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import QuestionList from '../../components/question/QuestionList';
+import ExcitingElephant from '../../components/icons/ExcitingElephant';
+import classes from './QuestionPage.module.css';
 
 const DUMMY_QUESTIONS = [
   {
@@ -26,7 +28,8 @@ const DUMMY_QUESTIONS = [
     id: 3,
     title: 'react가 뭐예요?',
     author: '리린이',
-    content: '자바스크립트만 들어봤어요.',
+    content: `항상 당연하게 create react app을 사용해서 react를 시작했는데, 오늘 보니 cra 안 하고 npm init부터 시작하시는 분들이 있더라구요. 
+    언제 cra를 사용하는 거죠? 아무리해봐도 모르겠어요 ㅠㅠ 제가 리액트를 잘 하게 되는 날이 올까요? 너무 힘들어요.ㅠㅠㅠㅠㅠㅠㅠㅠㅠ`,
     comments: [{ id: 1, author: '리프로', content: '공식문서 찾아보세요' }],
     createdAt: '2022-07-01',
     updatedAt: '2022-07-03',
@@ -37,10 +40,15 @@ const QuestionPage = () => {
   const navigate = useNavigate();
   const { roomId } = useParams(); 
   return (
-    <div>
-      QuestionPage
+    <div className={classes.questions}>
+      <header>
+        <ExcitingElephant />
+        <h3>질문 게시판</h3>
+      </header>
+      <button type="button" onClick={() => navigate(`/room/${roomId}/questions/new`)}>
+        + 질문 작성하기
+      </button>
       <QuestionList questions={DUMMY_QUESTIONS} />
-      <button type="button" onClick={() => navigate(`/room/${roomId}/questions/new`)}>질문하기</button>
     </div>
   );
 };
