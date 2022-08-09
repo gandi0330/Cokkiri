7428dfbd301dad6f72438e241358c57730e8b393
정혜령 <jhr3681@naver.com>
2022-08-09 20:16:04 +0900

    feat : S07P12C107-222 방목록 API 작업

--- frontend/src/components/rooms/VisitedLists.js
+++ frontend/src/components/rooms/VisitedLists.js
@@ -1,25 +1,28 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { MdOutlineKeyboardArrowDown as Arrow } from 'react-icons/md';
-import { fetchRecentRooms } from '../../store/roomListSlice';
+import { 
+  fetchRecentRooms, 
+  fetchFavoriteRooms, 
+  getRecentRooms,
+  getFavoriteRooms,
+} from '../../store/roomListSlice';
 import { getUserEmail } from '../../store/authSlice';
 
 import VisitiedListItem from './VisitedListItem';
 import classes from './VisitedLists.module.css';
 
-const visitedList = [
-  { id: 1, title: 'SSAFY 8기 대비' },
-  { id: 2, title: 'SSAFY' },
-  { id: 3, title: 'room3' },
-];
-
 const VisitedList = () => {
   const dispatch = useDispatch();
   const email = useSelector(getUserEmail);
+  const recentRooms = useSelector(getRecentRooms);
+  const favoriteRooms = useSelector(getFavoriteRooms);
 
   useEffect(() => {
+    if (!email) return;
+    dispatch(fetchFavoriteRooms({ email }));
     dispatch(fetchRecentRooms({ email }));
-  }, []);
+  }, [email]);
 
   return (
     <section className={classes.section}>
@@ -29,10 +32,22 @@ const VisitedList = () => {
       <input type="checkbox" name="droptdown-toggle" id="droptdown-toggle" />
       <div className={classes.hideDropdown}>
         <div className={classes.studyDropdown}>
-          {visitedList.length > 0 && visitedList.map((room) => (
-            <VisitiedListItem key={room.id} id={room.id} title={room.title} />
+          <span className={classes.studyList__subtitle}>최근 방문한 스터디</span>
+          {recentRooms.length === 0 && (
+            <p className={classes.dropdownMsg}>방문하신 스터디가 없습니다.</p>
+          )}
+          {recentRooms.length > 0 && recentRooms.map((room) => (
+            <VisitiedListItem type="recent" key={room.roomId} roomId={room.roomId} title={room.title} />
           ))}
-          {visitedList.length === 0 && <p className={classes.dropdownMsg}>방문하신 스터디가 없습니다.</p> }
+          {favoriteRooms.length > 0 && (
+            <>
+              <span className={classes.studyList__partition} />
+              <span className={classes.studyList__subtitle}>즐겨 찾는 스터디</span>
+            </>
+          )}
+          {favoriteRooms.length > 0 && favoriteRooms.map((room) => (
+            <VisitiedListItem type="favorite" key={room.roomId} roomId={room.roomId} title={room.title || ''} />
+          ))}
         </div>
       </div>
       <label htmlFor="droptdown-toggle"><Arrow /></label>
