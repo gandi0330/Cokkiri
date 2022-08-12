c9df8426eb4a8883f2a9e30559fdecf2fffec32a
Wise Programmer <67681207+gandi0330@users.noreply.github.com>
2022-08-12 14:46:46 +0900

    fix : S07P12C107-270 스터디타임 외래키 설정

--- backend/src/main/java/corinee/cokkiri/db/domain/StudyTime.java
+++ backend/src/main/java/corinee/cokkiri/db/domain/StudyTime.java
@@ -2,6 +2,8 @@ package corinee.cokkiri.db.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -17,6 +19,7 @@ public class StudyTime {
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "email")
+    @OnDelete(action= OnDeleteAction.CASCADE)
     private User user;
 
     private LocalDateTime startDatetime;
