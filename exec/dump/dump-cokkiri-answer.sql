3bb868f44333ac57fa7674867bb33319e9269e05
홍석호 <wow2867@naver.com>
2022-08-16 15:53:30 +0900

    Upload New File

---
+++ exec/dump/dump-cokkiri-answer.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: i7c107.p.ssafy.io    Database: cokkiri
+-- ------------------------------------------------------
+-- Server version	8.0.30-0ubuntu0.20.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `answer`
+--
+
+DROP TABLE IF EXISTS `answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `answer` (
+  `answer_id` bigint NOT NULL,
+  `code` varchar(3000) DEFAULT NULL,
+  `content` varchar(3000) DEFAULT NULL,
+  `create_datetime` datetime(6) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `question_id` bigint DEFAULT NULL,
+  `room_id` bigint DEFAULT NULL,
+  `answer_writer_email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`answer_id`),
+  KEY `FK8frr4bcabmmeyyu60qt7iiblo` (`question_id`),
+  KEY `FK5au2ajdj2ueao8n5yt9tlmhm0` (`room_id`),
+  KEY `FKsxqjw2241p9x3hphgujworn0b` (`answer_writer_email`),
+  CONSTRAINT `FK5au2ajdj2ueao8n5yt9tlmhm0` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`) ON DELETE CASCADE,
+  CONSTRAINT `FK8frr4bcabmmeyyu60qt7iiblo` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`) ON DELETE CASCADE,
+  CONSTRAINT `FKsxqjw2241p9x3hphgujworn0b` FOREIGN KEY (`answer_writer_email`) REFERENCES `user` (`email`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-16 15:50:11
