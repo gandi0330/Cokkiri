0bad2553cefc32e54cb5ed3b85c93f17b8ce3e68
홍석호 <wow2867@naver.com>
2022-08-18 16:36:06 +0900

    Replace dump-cokkiri-recent_room.sql

--- exec/dump/dump-cokkiri-recent_room.sql
+++ exec/dump/dump-cokkiri-recent_room.sql
@@ -34,6 +34,16 @@ CREATE TABLE `recent_room` (
   CONSTRAINT `FKnpgb5j54rn2s1ymweg6duvbyr` FOREIGN KEY (`email`) REFERENCES `user` (`email`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recent_room`
+--
+
+LOCK TABLES `recent_room` WRITE;
+/*!40000 ALTER TABLE `recent_room` DISABLE KEYS */;
+INSERT INTO `recent_room` VALUES (23,'2022-08-18 16:04:14.907287',21,'gandi0330@naver.com'),(55,'2022-08-18 16:07:52.040122',53,'gandi0330@naver.com'),(58,'2022-08-18 16:08:14.845473',56,'wow2867@naver.com'),(64,'2022-08-18 16:08:27.574798',62,'wjd10577@naver.com'),(70,'2022-08-18 16:09:25.242048',68,'wjd10577@naver.com'),(76,'2022-08-18 16:10:00.948796',74,'wow2867@naver.com'),(79,'2022-08-18 16:10:10.564812',77,'suaveh3681@gmail.com'),(82,'2022-08-18 16:11:15.638972',80,'suaveh3681@gmail.com'),(85,'2022-08-18 16:12:47.122374',83,'suaveh3681@gmail.com'),(87,'2022-08-18 16:20:11.426082',83,'wow2867@naver.com'),(89,'2022-08-18 16:13:17.137736',83,'wjd10577@naver.com'),(99,'2022-08-18 16:26:16.663931',83,'gandi0330@naver.com');
+/*!40000 ALTER TABLE `recent_room` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -44,4 +54,4 @@ CREATE TABLE `recent_room` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-08-16 15:50:50
+-- Dump completed on 2022-08-18 16:32:44
