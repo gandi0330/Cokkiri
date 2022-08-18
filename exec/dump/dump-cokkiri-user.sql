a7f39e120ebd51bb1ace65d106d5fffb27f15995
홍석호 <wow2867@naver.com>
2022-08-18 16:37:51 +0900

    Replace dump-cokkiri-user.sql

--- exec/dump/dump-cokkiri-user.sql
+++ exec/dump/dump-cokkiri-user.sql
@@ -30,6 +30,16 @@ CREATE TABLE `user` (
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('admin@admin.com','admin','a8d51fc6a058bfeacb77818d42d420ac1bf31529393a784ec60f7c2443047462',''),('coach@coach.com','coach','adfc8eeda474ae69f57114fb6367494d97694e99d1242d26661d824cdb7a6f62',NULL),('gandi0330@naver.com','hmhmhm','9f415ae6961d331e1c55c875caf769d4043108e7e83ea2ade4997b87d9ebf1a3',''),('suaveh3681@gmail.com','cokkiri','b0a538227cb08bdc0470e25d823871b7ebbab7d138eb292540f431c9d96dfa0f',''),('wjd10577@naver.com','garden','4e56a775f5efb35262fe4ad26feeb6e57b96abb6a4127aedaff2780efce258b9',''),('wow2867@naver.com','OngLa','ef6815925db2fe9b82571f03d717c096ee9db011fefe5e14ce0ebb5260297e34','');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -40,4 +50,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-08-16 15:51:19
+-- Dump completed on 2022-08-18 16:31:42
